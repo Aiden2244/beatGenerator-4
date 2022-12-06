ea034869bc7e7a52864f4991ce5c7cb89f528dba
Aiden2244 <aidenm2244@gmail.com>
2022-12-05 23:14:43 -0500

    Implemented a very minimal GUI

--- __main__.py
+++ __main__.py
@@ -1,10 +1,44 @@
-from generator import generate
+from generator import generate, GUI
+from tkinter import *
+from tkinter import ttk
 
-print("BEAT GENERATOR VERSION 1.0.0 | WRITTEN BY AIDEN MCCORMACK")
+if not GUI:
+    print("BEAT GENERATOR VERSION 1.0.0 | WRITTEN BY AIDEN MCCORMACK")
 
-while generate():
-    pass
+    while generate():
+        pass
 
-print("Thank you for using the beat generator!")
-print()
+    print("Thank you for using the beat generator!")
+    print()
+
+else:
+    class App:
+        def __init__(self):
+            self.root = Tk()
+            self.root.title("Beat Generator")
+            self.root.geometry("700x400")
+
+            self.mainframe = ttk.Frame(self.root)
+            self.mainframe.pack(fill='both', expand=True)
+
+            self.text = ttk.Label(self.mainframe, text="Beat Generator", font=("Helvetica", 60))
+            self.text.grid(row=1, column=1)
+
+            self.subtitle = ttk.Label(self.mainframe, text="A program by Aiden McCormack", font=("Helvetica", 18))
+            self.subtitle.grid(row=2, column=1)
+
+            self.generate_button = ttk.Button(self.mainframe, text="GENERATE BEAT", command=generate)
+            self.generate_button.grid(row=3, column=1)
+
+            self.dummy1 = ttk.Label(self.mainframe, text="")
+            self.dummy2 = ttk.Label(self.mainframe, text="")
+            self.dummy1.grid(row=0, column=0, padx=70, pady=10)
+            self.dummy2.grid(row=2, column=2, padx=60, pady=10)
+
+
+            self.root.mainloop()
+
+
+    if __name__ == "__main__":
+        App()
 
--- generator.py
+++ generator.py
@@ -2,57 +2,65 @@ from music import *
 from beat import Beat
 import os
 
+# CONTROL PANEL
+
+GUI = True  # enables the graphical user interface
+
+force_unique = True  # disables the repetition of chords in the beat progression if true
+force_root = True  # makes the first chord in the progression the tonic chord if true
+omit_diminished = True  # the progression will not include diminished chords if true
+random_instruments = True  # the instruments in each track will be set to their defaults if false
+octave = 0  # the octave offset of the beat, affects all tracks [-3, 5]
+
+# FOR EACH OF THE FOLLOWING, SET THE VALUE TO -1 FOR THE DEFAULT
+key = c  # the root note of the beat (c, db, d, eb, e, ... , bb, b OR any int [0, 11] )
+mode = -1  # the mode of the beat (0=major/1=minor)
+tempo = -1  # the tempo of the beat in bpm
+arp_pattern = -1  # specifies which arp pattern to use from the PATTERNS list in music.py [0,1]
+arp_duration = -1  # specifies the note duration of the melody and arpeggio
+bass_note_len = -1  # specifies the note duration of the bass note
+prog_len = -1  # the number of chords to include within the progression (if >7, disables force_unique)
+drum_mode = -1  # the mode to use for generating the drums [0, 1]
+num_phrases = -1  # the number of 4-measure loops to generate [1, ]
 
 def generate():
-    # CONTROL PANEL
-
-    force_unique = True  # disables the repetition of chords in the beat progression if true
-    force_root = True  # makes the first chord in the progression the tonic chord if true
-    omit_diminished = True  # the progression will not include diminished chords if true
-    random_instruments = True  # the instruments in each track will be set to their defaults if false
-    octave = 0  # the octave offset of the beat, affects all tracks [-3, 5]
-
-    # FOR EACH OF THE FOLLOWING, SET THE VALUE TO -1 FOR THE DEFAULT
-    key = -1  # the root note of the beat (c, db, d, eb, e, ... , bb, b OR any int [0, 11] )
-    mode = -1  # the mode of the beat (0=major/1=minor)
-    tempo = -1  # the tempo of the beat in bpm
-    arp_pattern = -1  # specifies which arp pattern to use from the PATTERNS list in music.py [0,1]
-    arp_duration = -1  # specifies the note duration of the melody and arpeggio
-    bass_note_len = -1  # specifies the note duration of the bass note
-    prog_len = -1  # the number of chords to include within the progression (if >7, disables force_unique)
-    drum_mode = -1  # the mode to use for generating the drums [0, 1]
-    num_phrases = -1  # the number of 4-measure loops to generate [1, ]
-
-    # Starts ui sequence
-    first_input = input("Press 'return' to generate a beat! (or type 't' to terminate) >> ")
-    if first_input == 't':
-        print("Terminated. No beat generated.")
-        return False
-    elif first_input == 'c':
-        print("Custom mode activated. Generating beat with hardcoded settings...")
-        __OUTPUT__ = Beat(
-                key=key,
-                mode=mode,
-                octave=octave,
-                tempo=tempo,
-                prog_len=prog_len,
-                bass_note_len=bass_note_len,
-                force_unique=force_unique,
-                force_root=force_root,
-                omit_diminished=omit_diminished,
-                random_instruments=random_instruments,
-                arp_pattern=arp_pattern,
-                arp_duration=arp_duration,
-                drum_mode=drum_mode,
-                num_phrases=num_phrases
-                )
-    else:
-        print("Generating...")
+    if GUI:
         __OUTPUT__ = Beat()
-    print("done!")
+        __OUTPUT__.info()
+        __OUTPUT__.write()
+        os.system("open -a GarageBand.app output.mid")
 
-    __OUTPUT__.info()
-    __OUTPUT__.write()
-    os.system("open -a GarageBand.app output.mid")
+    else:
+        # Starts ui sequence
+        first_input = input("Press 'return' to generate a beat! (or type 't' to terminate) >> ")
+        if first_input == 't':
+            print("Terminated. No beat generated.")
+            return False
+        elif first_input == 'c':
+            print("Custom mode activated. Generating beat with hardcoded settings...")
+            __OUTPUT__ = Beat(
+                    key=key,
+                    mode=mode,
+                    octave=octave,
+                    tempo=tempo,
+                    prog_len=prog_len,
+                    bass_note_len=bass_note_len,
+                    force_unique=force_unique,
+                    force_root=force_root,
+                    omit_diminished=omit_diminished,
+                    random_instruments=random_instruments,
+                    arp_pattern=arp_pattern,
+                    arp_duration=arp_duration,
+                    drum_mode=drum_mode,
+                    num_phrases=num_phrases
+                    )
+        else:
+            print("Generating...")
+            __OUTPUT__ = Beat()
+        print("done!")
 
-    return True
+        __OUTPUT__.info()
+        __OUTPUT__.write()
+        os.system("open -a GarageBand.app output.mid")
+
+        return True
